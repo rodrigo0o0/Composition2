69af8556e337b3440561d78066b76ef475f364b4
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-21 20:40:17 -0300

    Finish Project

--- Composition2/Entities/Comment.cs
+++ Composition2/Entities/Comment.cs
@@ -9,5 +9,11 @@ namespace Composition2.Entities
     internal class Comment
     {
         public string Text { get; set; }
+
+        public Comment() { }
+        public Comment(string text)
+        {
+            Text = text;
+        }
     }
 }
--- Composition2/Entities/Post.cs
+++ Composition2/Entities/Post.cs
@@ -12,12 +12,41 @@ namespace Composition2.Entities
         public string Title { get; set; }
         public string Content { get; set; }
         public int Likes { get; set; }
-        public List<Comment> comments { get; set; } = new List<Comment>();
+        public List<Comment> Comments { get; set; } = new List<Comment>();
 
-        
+        public Post() { }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
 
-        
-        
 
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.AppendLine(Likes + " Likes - "+ Moment );
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            foreach (Comment comment in Comments)
+            {
+                sb.AppendLine(comment.Text
+                    );
+            }
+            return sb.ToString();
+        }
     }
 }
