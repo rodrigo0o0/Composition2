d9e5fd9b21864179bad024a0e2328263bd972e59
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-21 20:12:04 -0300

    First Commit

---
+++ Composition2/Entities/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition2.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> comments { get; set; } = new List<Comment>();
+
+        
+
+        
+        
+
+    }
+}
